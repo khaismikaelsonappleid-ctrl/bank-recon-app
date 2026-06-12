d5676db6168399062f0074b717b524d8b4ac1280
khaismikaelsonappleid-ctrl <khaismikaelson+appleid@gmail.com>
2026-06-12 14:41:59 +0700

    Add language dictionary

---
+++ lib/dictionary.ts
@@ -0,0 +1,18 @@
+export const dictionary = {
+  en: {
+    date: 'Date',
+    time: 'Time',
+    items: 'Items',
+    amount: 'Amount',
+    balance: 'Balance',
+    channel: 'Channel',
+  },
+  fr: {
+    date: 'Date',
+    time: 'Heure',
+    items: 'Articles',
+    amount: 'Montant',
+    balance: 'Solde',
+    channel: 'Canal',
+  },
+};
